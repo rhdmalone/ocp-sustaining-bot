c437ab9c0387c5a9a9a8bce09ed3c8373d9142cb
Yash Ajgaonkar <yajgaonk@redhat.com>
2025-04-07 17:47:22 +0530

    Rename helpers to slack_helpers

--- slack_main.py
+++ slack_main.py
@@ -3,7 +3,7 @@ from slack_bolt.adapter.socket_mode import SocketModeHandler
 from config import config
 import re
 
-from helpers.helper_functions import (
+from slack_helpers.helper_functions import (
     handle_help,
     handle_create_rosa_cluster,
     handle_create_openstack_vm,
