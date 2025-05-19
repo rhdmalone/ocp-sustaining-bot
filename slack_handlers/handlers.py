3fbff3c92ae616b45623b5c73981394ab96f16c5
Deirdre Malone <dmalone@redhat.com>
2025-05-19 16:11:06 +0100

    aws: allow user to specify the --region parameter if required

--- slack_handlers/handlers.py
+++ slack_handlers/handlers.py
@@ -126,6 +126,7 @@ def handle_create_aws_vm(say, user, region, command_line):
                 ":hourglass_flowing_sand: Now processing your request for a Linux Instance... Please wait."
             )
 
+            region = command_params.get("region", region)
             # Create EC2 instance using the helper
             ec2_helper = EC2Helper(region=region)
             server_status_dict = ec2_helper.create_instance(
@@ -301,6 +302,7 @@ def helper_display_dict_output_as_table(instances_dict, print_keys, say):
 def handle_list_aws_vms(say, region, user, command_line):
     try:
         params_dict = get_dict_of_command_parameters(command_line)
+        region = params_dict.get("region", region)
 
         ec2_helper = EC2Helper(region=region)  # Set your region
         instances_dict = ec2_helper.list_instances(params_dict)
