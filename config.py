dee2e4cd4fe04c738e8c891e4b6ad876d718e92e
Aditya Khatavkar <akhatavk@redhat.com>
2025-06-30 12:15:27 +0100

    Vault integration

--- config.py
+++ config.py
@@ -1,63 +1,42 @@
 import logging
 import os
 from dotenv import load_dotenv
+from dynaconf import Dynaconf
+import tempfile
 
+required_keys = [
+    "SLACK_BOT_TOKEN",
+    "SLACK_APP_TOKEN",
+    "AWS_ACCESS_KEY_ID",
+    "AWS_SECRET_ACCESS_KEY",
+    "AWS_DEFAULT_REGION",
+    "OS_AUTH_URL",
+    "OS_PROJECT_ID",
+    "OS_INTERFACE",
+    "OS_ID_API_VERSION",
+    "OS_REGION_NAME",
+    "OS_APP_CRED_ID",
+    "OS_APP_CRED_SECRET",
+    "OS_AUTH_TYPE",
+    "ALLOW_ALL_WORKSPACE_USERS",
+    "ALLOWED_SLACK_USERS",
+]
 
-class Config:
-    """
-    Config class to load and validate environment variables.
+load_dotenv()
+ca_bundle_file = tempfile.NamedTemporaryFile()
+with open(ca_bundle_file.name, "w") as f:
+    f.write(os.getenv("RH_CA_BUNDLE_TEXT"))
 
-    If any environment variable is missing or empty, a ValueError will be raised.
-    """
+config = Dynaconf(
+    load_dotenv=True,
+    environment=False,
+    settings_files=["settings.toml", ".secrets.toml"],
+    vault_enabled=True,
+    vault={"url": "https://vault.corp.redhat.com:8200/", "verify": ca_bundle_file.name},
+)
 
-    def __init__(self):
-        # Load environment variables from a .env file
-        load_dotenv()
-        required_keys = [
-            "SLACK_BOT_TOKEN",
-            "SLACK_APP_TOKEN",
-            "AWS_ACCESS_KEY_ID",
-            "AWS_SECRET_ACCESS_KEY",
-            "AWS_DEFAULT_REGION",
-            "OS_AUTH_URL",
-            "OS_PROJECT_ID",
-            "OS_INTERFACE",
-            "OS_ID_API_VERSION",
-            "OS_REGION_NAME",
-            "OS_APP_CRED_ID",
-            "OS_APP_CRED_SECRET",
-            "OS_AUTH_TYPE",
-            "ALLOW_ALL_WORKSPACE_USERS",
-            "ALLOWED_SLACK_USERS",
-        ]
-        for key in required_keys:
-            value = os.getenv(key)
-            try:
-                if not value:
-                    # Raise an error if the environment variable is missing or empty
-                    raise ValueError(
-                        f"Missing or empty value for environment variable: {key}"
-                    )
-
-                setattr(self, key, value)
-
-            except ValueError as e:
-                logging.error(f"Error: {e}")
-                raise
-
-            except Exception as e:
-                logging.error(f"Unexpected error with environment variable {key}: {e}")
-                raise
-
-        log_level = os.getenv("LOG_LEVEL", "INFO")
-        self.log_level = log_level.upper()
-
-        self.setup_logging()
-
-    def setup_logging(self):
-        log_format = "[%(asctime)s %(levelname)s %(name)s] %(message)s"
-
-        logging.basicConfig(level=self.log_level, format=log_format)
-
-
-config = Config()
+# Verify that all keys were loaded correctly
+for k in required_keys:
+    if not hasattr(config, k):
+        logging.error(f"Could not read key: {k} from Vault.")
+        raise AttributeError(f"Could not read key: {k} from Vault.")
